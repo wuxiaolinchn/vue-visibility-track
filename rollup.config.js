dd3488c6ca182c09c5288965253cdcc7f08e81fa
wuxiaolinchn <wuxiaolinchn@outlook.com>
2020-04-04 20:33:22 +0800

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 import babel from 'rollup-plugin-babel';
 import resolve from "@rollup/plugin-node-resolve";
-import {version} from './package.json';
 
 export default {
     input: './src/index.js',
     output: {
-        file: `vue-visible-track.${version}.js`,
+        file: `dist/vue-visible-track.js`,
         name: 'VueVisibleTrack',
         format: 'umd'
     },
